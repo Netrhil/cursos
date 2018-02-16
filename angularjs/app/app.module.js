32873e9ca3014773c75d26e57f5ae10075120b4c
Netrhil <saragar.jim@gmail.com>
2018-02-15 23:29:17 -0300

    AngularJs: Agregando archivos de modulos y componentes
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

---
+++ angularjs/app/app.module.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('prueba',["ngRoute" ,"ngResource" ,"listaBlog"]);
---
+++ angularjs/lista-blog/lista-blog.component.js
@@ -0,0 +1,32 @@
+"use strict";
+
+angular.module("listaBlog").
+        component("listaBlog",{
+            templateUrl:"./templates/lista-blog.html",
+            controller :function($scope){ //el scope es obligatorio
+                
+                    var blogItems = [
+                        {id:1 ,titulo:"titulo 1" , descripcion: "descripcion 1"},
+                        {id:2 ,titulo:"titulo 2" , descripcion: "descripcion 2"},
+                        {id:3 ,titulo:"titulo 3" , descripcion: "descripcion 3"},
+                        {id:4 ,titulo:"titulo 4" , descripcion: "descripcion 4"},
+                        {id:5 ,titulo:"titulo 5" , descripcion: "descripcion 5"},
+                    ];
+                    $scope.items = blogItems;
+                    $scope.cosa = "0 Clicks!!"
+                    $scope.clicks = 0;
+                    
+                    /**
+                     * Las funciones dentro del scope del controlador
+                     * tambien se declaran aqui.
+                     */
+                    $scope.clickTest = function () {
+                        console.log( $scope.clicks);
+                        $scope.clicks ++;
+                        $scope.cosa = $scope.clicks + " Clicks!!";                
+                        
+                    }
+                }
+
+        });
+        

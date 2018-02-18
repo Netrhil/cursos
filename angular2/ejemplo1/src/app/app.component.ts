28995da2bcc04b28b32e99ea641e0122d02c2727
Netrhil <saragar.jim@gmail.com>
2018-02-18 18:26:48 -0300

    Angular2: ejercicio1 -> Checklist
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

--- angular2/ejemplo1/src/app/app.component.ts
+++ angular2/ejemplo1/src/app/app.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
 
+interface item {
+  detalle:string;
+  tachado:boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'alguien!';
-  visible = true;
-  pruebaClasesCss = 'red';
+  // nombre = 'alguien!';
+  // visible = true;
+  // pruebaClasesCss = 'red';
 
-  items = [
-    {cosa: 1 , check: true},
-    {cosa: 2 , check: true},
-    {cosa: 4 , check: true},
-    {cosa: 3 , check: true},
-  ]
-  cambiarNombre(nombreHtml: string) {
-    this.nombre = nombreHtml;
+  // items = [
+  //   {cosa: 1 , check: true},
+  //   {cosa: 2 , check: true},
+  //   {cosa: 4 , check: true},
+  //   {cosa: 3 , check: true},
+  // ]
+  // cambiarNombre(nombreHtml: string) {
+  //   this.nombre = nombreHtml;
+  // }
+  lista: item[] = [];
+  nombre: string;
+  
+  agregarToDo(nombreInput) {
+    this.lista.push({detalle: nombreInput, tachado: false });
+    this.nombre = "";
+  }
+
+  eliminarItem(item) {
+    this.lista.splice(item, 1);
   }
 }
 

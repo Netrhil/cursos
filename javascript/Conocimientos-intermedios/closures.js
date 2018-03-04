ade605e016a480228988485aac2f199397d51a8e
Netrhil <saragar.jim@gmail.com>
2018-03-04 11:12:09 -0300

    js: conocimientos intermedios y objetos
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

---
+++ javascript/Conocimientos-intermedios/closures.js
@@ -0,0 +1,21 @@
+function saludarFamilia(apellido) {
+    return function saludarMiembroDeFamilia(nombre) {
+      console.log(`Hola ${nombre} ${apellido}`)
+    }
+  }
+  
+  const saludarGomez = saludarFamilia("Gomez")
+  const saludarPerez = saludarFamilia("Perez")
+  const saludarRomero = saludarFamilia("Romero")
+  
+  saludarGomez("Pedro")
+  saludarGomez("Juan")
+  saludarGomez("Laura")
+  saludarGomez("Mónica")
+  
+  saludarPerez("Dario")
+  saludarPerez("Martin")
+  saludarPerez("Julieta")
+  
+  saludarRomero("Jorge")
+  

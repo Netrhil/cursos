f34dd970bbf1cc1b1b108d74777c741f6bfcd080
Netrhil <saragar.jim@gmail.com>
2018-03-25 15:47:51 -0300

    react: avanzes en botones del reproductor
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

--- React/react-curso/src/playlist/components/playlist.js
+++ React/react-curso/src/playlist/components/playlist.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Media from './media.js';
 import Play from '../../icons/components/play';
+import Volume from '../../icons/components/volume';
+import Pause from '../../icons/components/pause';
 
 
 //Componente funcional
@@ -10,6 +12,8 @@ function Playlist(props) {
         return (
             <div className="Playlist">
                 <Play color="red" size="60"/>
+                <Volume color="blue" size="60"/>
+                <Pause color="yellow" size="60"/>
                 {
                     playlist.map((item) => {
                         // En el caso que se llamen de igual manera

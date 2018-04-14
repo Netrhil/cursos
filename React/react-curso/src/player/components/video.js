ceebfe5ed5271bf3b77f45985a1f17afdee37fce
José Mansilla Lobos <jmansilla@latinshare.com>
2018-04-13 21:26:22 -0300

    React: Componente de controles + layout
    Signed-off-by: José Mansilla Lobos <jmansilla@latinshare.com>

--- React/react-curso/src/player/components/video.js
+++ React/react-curso/src/player/components/video.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import './video.css'
 
 class Video extends Component {
+    togglePlay() {
+        if(this.props.pause) {
+            // Elemento Referenciado al HTML
+            this.video.play();
+        } else {
+            this.video.pause();
+        }
+
+    }
+
+    // Recibe como parametro las proximas propiededes
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.pause != this.props.pause) {
+            this.togglePlay();
+        }
+
+    }
+
+    setRef = element => {
+        this.video = element;
+    }
+    
+    
     render() {
+        const {
+            handleLoadedMetadata
+        } = this.props
+
         return (
             <div>
                 <video
                     autoPlay={this.props.autoplay}
                     src={this.props.src}
+                    ref={this.setRef}
+                    onLoadedMetadata={this.props.handleLoadedMetadata}
+                  
                 />
             </div>
         )
--- React/react-curso/src/player/containers/video-player.js
+++ React/react-curso/src/player/containers/video-player.js
@@ -2,18 +2,56 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
+import Timer from '../components/timer';
+import Controls from '../components/video-player-controls';
 
 class VideoPlayer extends Component {
+    state = {
+        pause: true,
+        duration: 0
+    }
+
+    togglePlay = () => {
+        this.setState({
+            pause: !this.state.pause
+        })
+
+    }
+
+    componentDidMount = () => {
+      this.setState({
+          pause: !this.props.autoplay
+      })
+    }
+
+    handleLoadedMetadata = event => {
+        this.video = event.target;
+        this.setState({
+            duration: this.video.duration
+        });
+    }
+
     render() {
         return (
             <VideoPlayerLayout>
                 <Title
                     title="soy un titulo :D"
                 />
-
+                <Controls>
+                    <PlayPause
+                        pause={this.state.pause}
+                        handleClick={this.togglePlay}
+                    />
+                    <Timer 
+                        duration={this.state.duration}
+                    />
+                </Controls>
                 <Video 
+                    pause={this.state.pause}
                     autoplay={true}
                     src={"http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"}
+                    handleLoadedMetadata={this.handleLoadedMetadata}
                 />
               
             </VideoPlayerLayout>

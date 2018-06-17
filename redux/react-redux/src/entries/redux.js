25372b71d58336ba0ab6424569a10762a3a661de
Netrhil <saragar.jim@gmail.com>
2018-06-16 22:30:47 -0400

    redux-vanilla -> obteniendo datos del store
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

--- redux/react-redux/src/entries/redux.js
+++ redux/react-redux/src/entries/redux.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit);
 
@@ -7,6 +9,33 @@ function handleSubmit(event) {
     const data = new FormData($form);
     const title = data.get('title');
     console.log(title);
-    
-    
 }
+
+//inicializacion de store
+const initialState = [
+    {
+        "title" : "despacito",
+    },
+    {
+        "title" : "One more time",
+    },
+    {
+        "title" : "the same asylum as before",
+    },
+]
+
+const store = createStore(
+    (state) => state,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const $container = document.getElementById('playlist');
+const dataPlayList = store.getState();
+
+dataPlayList.forEach(item => {
+    const template = document.createElement('p');
+    template.textContent = item.title;
+    $container.appendChild(template);    
+});
+

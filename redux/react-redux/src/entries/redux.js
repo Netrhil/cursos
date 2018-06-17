9b6ca1166075d71c4c1e3e7b7693b1c0af344ff7
Netrhil <saragar.jim@gmail.com>
2018-06-16 22:59:12 -0400

    redux-vanilla -> creando reducer para agregar cancion
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

--- redux/react-redux/src/entries/redux.js
+++ redux/react-redux/src/entries/redux.js
@@ -8,7 +8,13 @@ function handleSubmit(event) {
     //Clase para manipular el form
     const data = new FormData($form);
     const title = data.get('title');
-    console.log(title);
+   
+    store.dispatch({
+        type: 'ADD_SONG',
+        payload: {
+            title 
+        }
+    });
 }
 
 //inicializacion de store
@@ -24,8 +30,18 @@ const initialState = [
     },
 ]
 
+const reducer = function(state, action) {
+    switch (action.type) {
+        case 'ADD_SONG':
+            return [...state, action.payload];            
+        default:
+            return state;
+    }
+    
+}
+
 const store = createStore(
-    (state) => state,
+    reducer,
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )

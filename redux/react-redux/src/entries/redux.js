a7cbc4f1064d5c180dd49c97cb4929ef731540d6
Netrhil <saragar.jim@gmail.com>
2018-06-30 20:43:33 -0400

    react-redux: agregado provider a la aplicacion
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

--- redux/react-redux/src/entries/redux.js
+++ redux/react-redux/src/entries/redux.js
@@ -4,67 +4,62 @@ const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
-    //Clase para manipular el form
-    const data = new FormData($form);
-    const title = data.get('title');
-    
-    //accion que es procesada por el reducer para agregar cancion
-    store.dispatch({
-        type: 'ADD_SONG',
-        payload: {
-            title 
-        }
-    });
+  event.preventDefault();
+  const data = new FormData($form);
+  const title = data.get('title');
+  console.log(title);
+  store.dispatch({
+    type: 'ADD_SONG',
+    payload: {
+      title,
+    }
+  })
 }
-
-//inicializacion de store
 const initialState = [
-    {
-        "title" : "despacito",
-    },
-    {
-        "title" : "One more time",
-    },
-    {
-        "title" : "the same asylum as before",
-    },
+  {
+    "title": "Despacito",
+  },
+  {
+    "title": "One more time",
+  },
+  {
+    "title": "Echame la culpa",
+  }
 ]
 
+
 const reducer = function(state, action) {
-    switch (action.type) {
-        case 'ADD_SONG':
-            return [...state, action.payload];            
-        default:
-            return state;
-    }
-    
+  switch (action.type) {
+    case 'ADD_SONG':
+      return [...state, action.payload]
+    default:
+      return state
+  }
 }
 
 const store = createStore(
-    reducer,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
-
 function render() {
-    const $container = document.getElementById('playlist');
-    const dataPlayList = store.getState();
-    $container.innerHTML = '';
-
-    dataPlayList.forEach(item => {
-        const template = document.createElement('p');
-        template.textContent = item.title;
-        $container.appendChild(template);    
-    });
+  const $container = document.getElementById('playlist');
+  const playlist = store.getState();
+  $container.innerHTML = '';
+  playlist.forEach((item) => {
+    const template = document.createElement('p');
+    template.textContent = item.title;
+    $container.appendChild(template);
+  })
 }
-
 render();
 
 function handleChange() {
-    render();
+  render();
 }
 
-store.subscribe(handleChange);
+store.subscribe(handleChange)
+
+// console.log(store.getState())
 
